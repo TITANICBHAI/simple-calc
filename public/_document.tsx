0fc28f377c7e75a8e8a92f5ee500c7f35107d35e
TITANIC BHAI <himanshusmartwatch@gmail.com>
2025-06-18 09:26:23 +0530

    Create _document.tsx

---
+++ public/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="google-site-verification" content="p_nM5UQGi4P-XxZWzkNBNK0htFxIVIZKhVT6H9Q4WJY" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
